8e56e8ba60a9a5432d6fdd4c0519275d45e7a23d
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-07 13:57:17 +0600

    update todo app completed file

--- todoapp/js/script.js
+++ todoapp/js/script.js
@@ -1,1 +1,60 @@
-/* JavaScript Goes Here */
+/* JavaScript TO DO App */
+console.log("Project To Do");
+
+function addTask() {
+  let taskInputElem = document.querySelector("#taskInput");
+  let ultaskListElem = document.querySelector("#taskList");
+
+  let task = taskInputElem.value;
+  if (task.trim() == "") return;
+
+  liElem = document.createElement("li");
+  liElem.innerText = task;
+
+  selectBtnElem = document.createElement("button");
+  selectBtnElem.innerText = "Select";
+  selectBtnElem.classList.add("buttonGap");
+  selectBtnElem.onclick = function () {
+    liElem.classList.toggle("completed");
+  };
+
+  liElem.appendChild(selectBtnElem);
+
+  deleteBtnElem = document.createElement("button");
+  deleteBtnElem.innerText = "Delete";
+  deleteBtnElem.classList.add("buttonGap");
+  deleteBtnElem.onclick = function () {
+    liElem.remove();
+  };
+
+  liElem.appendChild(deleteBtnElem);
+
+  ultaskListElem.appendChild(liElem);
+  taskInputElem.value = " ";
+
+  //console.log(ultaskListElem);
+}
+//addTask();
+
+/* function deleteTask() {
+  liElem.remove();
+} */
+
+function filterTasks() {
+  let inputElem = document.querySelector("#searchInput");
+  let inputValue = inputElem.value;
+
+  let itemListElem = document.querySelectorAll("ul#taskList li");
+  itemListElem.forEach((item) => {
+    /* item.style.backgroundColor = item.innerText
+        .toLowerCase()
+        .includes(inputValue.toLowerCase())
+        ? "green"
+        : ""; */
+    item.style.display = item.innerText
+      .toLowerCase()
+      .includes(inputValue.toLowerCase())
+      ? "block"
+      : "none";
+  });
+}
