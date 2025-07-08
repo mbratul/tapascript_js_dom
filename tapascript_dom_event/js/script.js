0d011de87529f151ae27a277b0529cc115f1feda
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-08 11:01:55 +0600

    update event listener

--- tapascript_dom_event/js/script.js
+++ tapascript_dom_event/js/script.js
@@ -12,8 +12,44 @@ myBtn2Elem.onclick = function () {
 };
 
 let countButtonElem = document.querySelector("#countButton");
+
 let counter = 0;
-countButtonElem.addEventListener("click", function () {
+let handleCount = function () {
+  console.log("Counter", counter);
+  counter++;
+};
+let greetMe = function () {
+  console.log("Thank you", counter);
+  counter++;
+};
+// countButtonElem.addEventListener("click", function () {
+//   console.log("counter", counter);
+//   counter++;
+// });
+countButtonElem.addEventListener("click", handleCount); // add event listener
+countButtonElem.addEventListener("click", greetMe);
+/* countButtonElem.removeEventListener("click", function () {
   console.log("counter", counter);
   counter++;
+}); */
+countButtonElem.removeEventListener("click", handleCount); //remove event listener
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("dom content loaded");
 });
+
+//Event Object
+let searchElem = document.querySelector("#searchInput");
+
+function handleChange(event) {
+  console.log(event);
+  console.log("Target", event.target);
+  console.log("Target Name", event.target.name);
+  console.log("Target Value", event.target.value);
+  console.log("Event Type", event.type);
+  console.log("Current Target", event.currentTarget);
+  console.log("this :", this);
+
+  searchElem.value = " ";
+}
+searchElem.addEventListener("change", handleChange);
