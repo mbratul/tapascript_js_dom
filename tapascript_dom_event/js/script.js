c1f7d4eebf90d3e8f0acbb2ade844441572517bb
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-08 07:26:46 +0600

    addEventelistener function added

--- tapascript_dom_event/js/script.js
+++ tapascript_dom_event/js/script.js
@@ -1,5 +1,19 @@
 /* JavaScript Goes Here */
 
-function handleClick() {
-  console.log("button click");
+// Event Handling in Markup
+function handleClick(greeting) {
+  console.log(`button click with a message ${greeting}`);
 }
+
+// Event Handling in Script
+let myBtn2Elem = document.querySelector("#myBtn2");
+myBtn2Elem.onclick = function () {
+  console.log(`my button 2 click by me`);
+};
+
+let countButtonElem = document.querySelector("#countButton");
+let counter = 0;
+countButtonElem.addEventListener("click", function () {
+  console.log("counter", counter);
+  counter++;
+});
