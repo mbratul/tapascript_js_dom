4ec18f146e6d00863d588ef2db65353e6deab5c6
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-09 12:49:49 +0600

    mini faq project added

---
+++ tapascript_dom_event/mini-faq/faq.js
@@ -0,0 +1,18 @@
+let faq = document.querySelector(".faq");
+// console.log(faq);
+//add event listener
+faq.addEventListener("click", function (e) {
+  if (e.target.classList.contains("question")) {
+    e.stopPropagation();
+    let currentItem = e.target.parentElement;
+    let currentAnswer = currentItem.querySelector(".answer");
+    currentAnswer.classList.toggle("show");
+  }
+
+  //console.log(currentAnswer);
+});
+
+document.addEventListener("click", function () {
+  allAnswer = document.querySelectorAll(".answer.show");
+  allAnswer.forEach((answer) => answer.classList.remove("show"));
+});
