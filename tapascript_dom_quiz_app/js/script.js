b35887e56b36f5d4fbb3d8b52544742c44010ecc
Muhtasim Billah Ratul <mbratul@gmail.com>
2025-07-19 17:14:10 +0600

    dom quiz app completed

--- tapascript_dom_quiz_app/js/script.js
+++ tapascript_dom_quiz_app/js/script.js
@@ -31,6 +31,9 @@ const quizDataElem = [
 
 let questions = [...quizDataElem].sort(() => Math.random() - 0.5);
 let currentQuestion = 0;
+let score = 0;
+let timer;
+let timeLeft;
 
 const questionElem = document.getElementById("question");
 const optionsElem = document.getElementById("options");
@@ -39,30 +42,75 @@ const timerElem = document.getElementById("timer");
 const resultElem = document.getElementById("result");
 
 function loadQuestion() {
+  clearInterval(timer);
+  timeLeft = 15;
+  updateTimer();
+  timer = setInterval(countdown, 1000);
   const q = questions[currentQuestion];
   questionElem.textContent = `Q${currentQuestion + 1}. ${q.question}`;
+  optionsElem.innerHTML = "";
 
   q.options.forEach((option, index) => {
     const btn = document.createElement("button");
     btn.classList.add("option-btn");
     btn.textContent = option;
-    btn.addEventListener("click", () => selectAnswer(index));
+    btn.addEventListener("click", () => selectAnswer(index, true));
     optionsElem.appendChild(btn);
   });
   nextBtnElem.style.display = "none";
 }
 
-function selectAnswer(index) {
-  const q = questionElem[currentQuestion];
-  const button = document.querySelectorAll(".option-btn");
-  button.forEach((btn) => (btn.disabled = true));
+function countdown() {
+  timeLeft--;
+  updateTimer();
+
+  if (timeLeft === 0) {
+    clearInterval(timer);
+    selectAnswer(questions[currentQuestion]?.correct, false);
+  }
+}
+
+function updateTimer() {
+  timerElem.textContent = `${timeLeft} Second`;
+}
+function selectAnswer(index, shouldScore) {
+  clearInterval(timer);
+  const q = questions[currentQuestion];
+
+  const buttons = document.querySelectorAll(".option-btn");
+  buttons.forEach((btn) => (btn.disabled = true));
   if (index === q.correct) {
-    button[index].classList.add("correct");
+    shouldScore && score++;
+    buttons[index].classList.add("correct");
   } else {
-    button[index].classList.add("wrong");
-    button[q.correct].classList.add("correct");
+    buttons[index].classList.add("wrong");
+    buttons[q.correct].classList.add("correct");
   }
   nextBtnElem.style.display = "inline-block";
 }
+nextBtnElem.addEventListener("click", () => {
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    loadQuestion();
+  } else {
+    showResult();
+  }
+});
 
+function showResult() {
+  nextBtnElem.style.display = "none";
+  const highScore = localStorage.getItem("quizHighScore") || 0;
+  const isNew = score > highScore;
+
+  if (isNew) {
+    localStorage.setItem("quizHighScore", score);
+  }
+  resultElem.innerHTML = `
+    <ht>Hurray!! quiz complete</h2>
+    <p>you have scored ${score} out of ${questions.length} questions</p>
+    <p>Highest Score: ${Math.max(score, highScore)}</p>
+    ${isNew ? "<p> hey new high score</p>" : ""}
+    <button onclick="location.reload()">Restart the Quiz</button>
+  `;
+}
 loadQuestion();
